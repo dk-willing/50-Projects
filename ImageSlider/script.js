edebaa0dfddd6f0483a19da10e0d549b806a1314
dk-willing <davidkarikari89@gmail.com>
2025-05-16 22:24:43 +0000

    Added 28th branch

--- ImageSlider/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const smallImages = document.querySelectorAll('.small-img-box img');
-const largeImage = document.querySelector('.big-img-box img');
-
-smallImages.forEach(img => {
-  img.addEventListener('click', () => (largeImage.src = img.src));
-});
