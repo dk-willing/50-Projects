34dcbb9f4899ceb294c51b01cd3dbeb107bb7da8
dk-willing <davidkarikari89@gmail.com>
2025-05-31 11:04:45 +0000

    Use API to get country details

---
+++ Async-Project/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+// NEW COUNTRIES API URL (use instead of the URL shown in videos):
+// https://restcountries.com/v2/name/portugal
+
+// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
+
+function getCountry(country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const countryCurrencyCode = Object.keys(data.currencies)[0];
+    const { name, symbol } = data.currencies[countryCurrencyCode];
+
+    const htmlMarkUp = `
+  <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${name} - ${symbol}</p>
+          </div>
+    </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', htmlMarkUp);
+    countriesContainer.style.opacity = 1;
+  });
+}
+
+getCountry('ghana');
+getCountry('nigeria');
+getCountry('spain');
+getCountry('america');
+getCountry('brazil');
+getCountry('guinea');
